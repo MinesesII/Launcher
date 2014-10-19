39b6824a44af2cc41cb3f3d6e2507a1e06225f60
MinesesII <sonicyugi@gmail.com>
2014-10-19 18:37:17 +0200

    REGLAGE BUG

--- src/ProgressBar.java
+++ src/ProgressBar.java
@@ -2,24 +2,15 @@ import java.awt.*;
 
 import javax.swing.*;  
 
-import java.util.*;  
-import java.awt.event.*;  
-
 @SuppressWarnings("serial")
 public class ProgressBar extends JPanel  
-implements MouseListener{  
+{  
 
 	private float progress=0;
-	
-	@SuppressWarnings("unused")
-	private Vector<ActionListener> listeners = null;  
-	boolean hit = false;  
 	Image image = null;  
 
 	public ProgressBar (Image image){  
 		super();  
-		listeners = new Vector<ActionListener>();  
-		addMouseListener(this);  
 		this.image = image;  
 	}  
 
@@ -28,7 +19,6 @@ implements MouseListener{
 	}  
 
 	public void paintComponent(Graphics g){  
-		g.setColor(getParent().getBackground());  
 		Graphics2D g2D = (Graphics2D)g;  
 		g2D.setClip(0, 0, (int)(getWidth()*progress), getHeight());
 		g2D.drawImage(image,0,0, getWidth(),getHeight(),this); 
@@ -38,24 +28,4 @@ implements MouseListener{
 		progress = i;
 		repaint();
 	}
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {		
-	}  
 }
--- src/Windows.java
+++ src/Windows.java
@@ -12,33 +12,30 @@ public class Windows extends JFrame
 {
 
 	private ProgressBar progressBar;
-	
+
 	public Windows()
 	{
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-				} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-				}
-				setLayout(null);
-				setUndecorated(true);
-				setSize(800, 600);
-				setLocationRelativeTo(null);
-				setBackground(new Color(0,0,0,0));
-				setContentPane(new JLabel(new ImageIcon(getClass().getResource("background.png"))));
-				MouseAdapter mouseHandler = new MouseAdapter() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+		}
+		setLayout(null);
+		setUndecorated(true);
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setBackground(new Color(0,0,0,0));
+		setContentPane(new JLabel(new ImageIcon(getClass().getResource("background.png"))));
+		MouseAdapter mouseHandler = new MouseAdapter() {
 
-					private Point offset;
+			private Point offset;
 
-					protected boolean isWithinBorder(MouseEvent e) {
-						Point p = e.getPoint();
-						Component comp = e.getComponent();
-						return p.x < 79 || p.y < 168 || p.x > comp.getWidth() - 85 || p.y > comp.getHeight()  - 73;
-					}
+			protected boolean isWithinBorder(MouseEvent e) {
+				Point p = e.getPoint();
+				Component comp = e.getComponent();
+				return p.x < 79 || p.y < 168 || p.x > comp.getWidth() - 85 || p.y > comp.getHeight()  - 73;
+			}
 
-					/*   @Override
+			/*   @Override
 	                    public void mouseMoved(MouseEvent e) {
 	                    	  Component comp = e.getComponent();
 		                        if (isWithinBorder(e)) {
@@ -48,74 +45,73 @@ public class Windows extends JFrame
 		                        }
 	                    }*/
 
-					@Override
-					public void mousePressed(MouseEvent e) {
-						offset=null;
-						if (isWithinBorder(e)) {
-							Point pos = e.getComponent().getLocationOnScreen();
-							offset = new Point(e.getLocationOnScreen());
-							offset.x -= pos.x;
-							offset.y -= pos.y;
-						}
-					}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				offset=null;
+				if (isWithinBorder(e)) {
+					Point pos = e.getComponent().getLocationOnScreen();
+					offset = new Point(e.getLocationOnScreen());
+					offset.x -= pos.x;
+					offset.y -= pos.y;
+				}
+			}
 
-					@Override
-					public void mouseDragged(MouseEvent e) {
-						if (offset != null) {
-							Point pos = e.getLocationOnScreen();
-							int x = pos.x - offset.x;
-							int y = pos.y - offset.y;
-							SwingUtilities.getWindowAncestor(e.getComponent()).setLocation(x, y);
-						}
-					}
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				if (offset != null) {
+					Point pos = e.getLocationOnScreen();
+					int x = pos.x - offset.x;
+					int y = pos.y - offset.y;
+					SwingUtilities.getWindowAncestor(e.getComponent()).setLocation(x, y);
+				}
+			}
 
-				};
+		};
 
-				getContentPane().addMouseListener(mouseHandler);
-				getContentPane().addMouseMotionListener(mouseHandler);
-				Button closeButton = new Button("", new ImageIcon(getClass().getResource("crossUnclick.jpg")).getImage(), new ImageIcon(getClass().getResource("crossClick.jpg")).getImage());
-				Dimension borderButtonsize = closeButton.getPreferredSize();
-				closeButton.setBounds(744 , 30, borderButtonsize.width, borderButtonsize.height);
-				closeButton.addActionListener(new ActionListener(){  
-					public void actionPerformed(ActionEvent e) {
-						System.exit(0);
-					}
-				});
-				Button minimizeButton = new Button("", new ImageIcon(getClass().getResource("reduceUnclick.jpg")).getImage(), new ImageIcon(getClass().getResource("reduceClick.jpg")).getImage());
-				minimizeButton.setBounds(660 , 30, borderButtonsize.width, borderButtonsize.height);
-				minimizeButton.addActionListener(new ActionListener(){  
-					public void actionPerformed(ActionEvent e) {
-						setState(Frame.ICONIFIED);
-					}
-				});
-				Button playButton = new Button("", new ImageIcon(getClass().getResource("playUnclick.png")).getImage(), new ImageIcon(getClass().getResource("playClick.png")).getImage());
-				Dimension playButtonsize = playButton.getPreferredSize();
-				playButton.setBounds(550 , 460, playButtonsize.width, playButtonsize.height);
-				playButton.addActionListener(new ActionListener(){  
-					public void actionPerformed(ActionEvent e) {
-						if(!new File("Voxelion.jar").exists()){
-							new DownloadGame(Main.gameDownloadLink, "Voxelion.jar");
-						}
-						try {
-							new Loader().runNewLauncher("Voxelion.jar");
-						} catch (Exception e1) {
-							e1.printStackTrace();
-						}
-					}
-				});
-				progressBar = new ProgressBar(new ImageIcon(getClass().getResource("progressBar.png")).getImage());
-				Dimension progressBarSize = progressBar.getPreferredSize();
-				progressBar.setBounds(121 , 475, progressBarSize.width, progressBarSize.height);
-				getContentPane().add(closeButton);
-				getContentPane().add(minimizeButton);
-				getContentPane().add(playButton);
-				getContentPane().add(progressBar);
-				setLocationRelativeTo(null);
-				setVisible(true);
+		getContentPane().addMouseListener(mouseHandler);
+		getContentPane().addMouseMotionListener(mouseHandler);
+		Button closeButton = new Button("", new ImageIcon(getClass().getResource("crossUnclick.jpg")).getImage(), new ImageIcon(getClass().getResource("crossClick.jpg")).getImage());
+		Dimension borderButtonsize = closeButton.getPreferredSize();
+		closeButton.setBounds(744 , 30, borderButtonsize.width, borderButtonsize.height);
+		closeButton.addActionListener(new ActionListener(){  
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
 			}
 		});
+		Button minimizeButton = new Button("", new ImageIcon(getClass().getResource("reduceUnclick.jpg")).getImage(), new ImageIcon(getClass().getResource("reduceClick.jpg")).getImage());
+		minimizeButton.setBounds(660 , 30, borderButtonsize.width, borderButtonsize.height);
+		minimizeButton.addActionListener(new ActionListener(){  
+			public void actionPerformed(ActionEvent e) {
+				setState(Frame.ICONIFIED);
+			}
+		});
+		Button playButton = new Button("", new ImageIcon(getClass().getResource("playUnclick.png")).getImage(), new ImageIcon(getClass().getResource("playClick.png")).getImage());
+		Dimension playButtonsize = playButton.getPreferredSize();
+		playButton.setBounds(550 , 460, playButtonsize.width, playButtonsize.height);
+		playButton.addActionListener(new ActionListener(){  
+			public void actionPerformed(ActionEvent e) {
+				if(!new File("Voxelion.jar").exists()){
+					new DownloadGame(Main.gameDownloadLink, "Voxelion.jar");
+				}
+				try {
+					new Loader().runNewLauncher("Voxelion.jar");
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		progressBar = new ProgressBar(new ImageIcon(getClass().getResource("progressBar.png")).getImage());
+		Dimension progressBarSize = progressBar.getPreferredSize();
+		progressBar.setBounds(121 , 475, progressBarSize.width, progressBarSize.height);
+		getContentPane().add(closeButton);
+		getContentPane().add(minimizeButton);
+		getContentPane().add(playButton);
+		getContentPane().add(progressBar);
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
-	
+
+
 	public ProgressBar getProgressBar(){
 		return progressBar;
 	}
